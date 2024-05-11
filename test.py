7cef1697953c042587233da705c1d7268d2ca395
stvishw <stvishw@gmail.com>
2024-05-12 02:09:02 +0530

    new lines added for testing

--- test.py
+++ test.py
@@ -4,4 +4,20 @@ import datetime
 
 print ("First jenkins automation project")
 print ("automation")
+print ()
+
+#sudo docker cp 6e69cbf2086d://var/jenkins_home/workspace/Job-git/*  ~/Desktop/geeksforgeeks.txt
+
+#sudo docker cp 6e69cbf2086d://var/jenkins_home/workspace/Job-git ~/devops/
+
+#docker cp 6e69cbf2086d://var/jenkins_home/workspace/Job-git/*  ~/devops/
+
+#sudo docker cp ~/devops/transfer.sh 6e69cbf2086d:/tmp/to-be-copied.txt
+
+#sudo docker cp ~/devops/transfer.sh 6e69cbf2086d:/tmp/
+
+#/home/status/devops/
+#/var/jenkins_home/workspace/Job-git
+
+#sudo docker exec -it 6e69cbf2086d bash
 
